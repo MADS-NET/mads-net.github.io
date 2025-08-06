a8f8c131e9320e63d19fa27cabd85e6616664625
Paolo Bosetti <paolo.bosetti@unitn.it>
2025-08-06 10:52:18 +0200

    SVG logo shortcode

---
+++ _extensions/pbosetti/logo/logo.lua
@@ -0,0 +1,20 @@
+return {
+  ['logo'] = function(args, kwargs, meta, raw_args, context) 
+    local file = args[1] or "logo.svg"
+    local height = pandoc.utils.stringify(kwargs["height"])
+    if height == "" then
+      height = "0.9"
+    end
+    local offset = pandoc.utils.stringify(kwargs["offset"])
+    if offset == "" then
+      offset = "-0.125"
+    end
+    local folder = pandoc.utils.stringify(kwargs["folder"])
+    if folder == "" then
+      folder = "/images"
+    end
+    
+    local style = 'style="height: ' .. height .. 'em; vertical-align: ' .. offset .. 'em; margin: 0; padding: 0;"'
+    return pandoc.RawBlock("html", '<img src="' .. folder .. '/' .. file .. '" ' .. style .. '>')
+  end
+}
